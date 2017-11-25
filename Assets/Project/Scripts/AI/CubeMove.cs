4bb537f4e70c489bc284b3befa3ed9ba86c04330
977567460 <977567460@qq.com>
2017-11-25 11:51:01 +0800

    资源+aI
    AI+模型

---
+++ Assets/Project/Scripts/AI/CubeMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour {
+
+	// Use this for initialization
+    private float speed = 5;
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+          
+            transform.Translate(Vector3.forward *speed* Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+          
+            transform.Translate(Vector3.back *speed* Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+           
+           transform.Translate(Vector3.left *speed* Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+           
+            transform.Translate(Vector3.right *speed* Time.deltaTime);
+        }
+    }
+
+}
